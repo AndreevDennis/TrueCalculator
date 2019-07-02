894c8b4fb5ef44e8c72462f607eec846dd53901e
DennisAndreev <denisandreev1611@gmail.com>
2019-07-02 14:44:27 +0300

    Initial Commit

---
+++ TrueCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrueCalculator
+//
+//  Created by Денис Андреев on 7/2/19.
+//  Copyright © 2019 Денис Андреев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
